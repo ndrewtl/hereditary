8a0d1908dee4cf7ae42e061b67325fe98f1c32d7
Andrew Lee <andrew.lambda@tuta.io>
2021-08-25 23:40:01 -0400

    chore: more cleanup

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('example test', () => {
-  render(<App />);
-  // TODO write tests here
-});
