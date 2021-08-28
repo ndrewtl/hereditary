9239557d675307d0ec475ec18433d956adbc7772
Andrew Lee <andrew.lambda@tuta.io>
2021-08-28 02:22:12 -0400

    feat: add types

---
+++ src/utils/types.ts
@@ -0,0 +1,59 @@
+import { SimulationNodeDatum, SimulationLinkDatum } from 'd3-force';
+
+/**
+ * The ID for a person
+ */
+export type Identifier = string;
+
+
+/**
+ * Colors, used for styling
+ */
+export type Color = string;
+
+/**
+ * The country from which someone hails
+ */
+export type Country = string;
+
+/**
+ * A person can be
+ * - the current monarch
+ * - a previously-reigning monarch
+ * - not a monarch, or
+ * - expected to rule
+ */
+export enum Reign {
+    Current = 'current',
+    Past = 'past',
+    None = 'none',
+    Expected = 'expected'
+}
+
+/**
+ * A person is a SimulationNodeDatum with the attributes of a person
+ */
+export interface Person extends SimulationNodeDatum {
+  id: Identifier;
+  mother?: Identifier;
+  father?: Identifier;
+  country?: Country;
+  born: number;
+  reign?: Reign;
+};
+
+/**
+ * A link between people
+ */
+export type PersonLink = SimulationLinkDatum<Person>;
+
+/**
+ * One final type: this is the schema for the /public/data.yml file and is used for importing it
+ */
+export interface DataSchema {
+  people: [Person];
+  colors: {
+    countries: Record<Country, Color>;
+    reign: Record<Reign, Color>;
+  }
+};
