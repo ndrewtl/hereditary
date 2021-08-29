d5681592d2f31de750818e2d167fbc689e10faca
Andrew Lee <andrew.lambda@tuta.io>
2021-08-29 00:50:40 -0700

    chore: yarn lint --fix

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -11,44 +11,52 @@ import {
 } from '../utils/types';
 import { ageOrdering, fetchData } from '../utils/util';
 
-
 interface PersonSVGProps {
   person: Person;
   radius: number;
   reignColors: Record<Reign, Color>;
 }
 function PersonSVG({ person, radius, reignColors }: PersonSVGProps) {
-  const { id, name, country, reign, x, y} = person;
+  const {
+    id, name, country, reign, x, y
+  } = person;
 
-  return (<g>
-    <clipPath id={`${id}-mask`}>
-      <circle r={radius} cx={x!} cy={y!} />
-    </clipPath>
-    <image key={id}
-      href={`/flags/${country!}.svg`}
-      x={x! - 2*radius}
-      y={y! - radius}
-      height={radius * 2}
-      clipPath={`url(#${id}-mask)`} />
-    <circle r={radius}
-      cx={x!}
-      cy={y!}
-      stroke={reign ? reignColors[reign] : reignColors.none}
-      strokeWidth='4px'
-      fillOpacity='0'
-      id={id}
-      // cursor='move'
-    />
-    <text x={x}
-      y={y! - (1.2 * radius)}
-      textAnchor='middle'
-      stroke='#fff'
-      strokeWidth='0.5'
-      strokeOpacity='0.6'
-      fill='#000'
-      fontSize='16px'
-    >{name}</text>
-  </g>);
+  return (
+    <g>
+      <clipPath id={`${id}-mask`}>
+        <circle r={radius} cx={x!} cy={y!} />
+      </clipPath>
+      <image
+        key={id}
+        href={`/flags/${country!}.svg`}
+        x={x! - 2 * radius}
+        y={y! - radius}
+        height={radius * 2}
+        clipPath={`url(#${id}-mask)`}
+      />
+      <circle
+        r={radius}
+        cx={x!}
+        cy={y!}
+        stroke={reign ? reignColors[reign] : reignColors.none}
+        strokeWidth="4px"
+        fillOpacity="0"
+        id={id}
+      />
+      <text
+        x={x}
+        y={y! - (1.2 * radius)}
+        textAnchor="middle"
+        stroke="#fff"
+        strokeWidth="0.5"
+        strokeOpacity="0.6"
+        fill="#000"
+        fontSize="16px"
+      >
+        {name}
+      </text>
+    </g>
+  );
 }
 
 interface PersonLinkSVGProps {
@@ -56,13 +64,15 @@ interface PersonLinkSVGProps {
 }
 function PersonLinkSVG({ link }: PersonLinkSVGProps) {
   return (
-    <line x1={(link.source as Person).x}
-          y1={(link.source as Person).y}
-          x2={(link.target as Person).x}
-          y2={(link.target as Person).y}
-          stroke='#999'
-          strokeOpacity='0.6'
-          strokeWidth='10' />
+    <line
+      x1={(link.source as Person).x}
+      y1={(link.source as Person).y}
+      x2={(link.target as Person).x}
+      y2={(link.target as Person).y}
+      stroke="#999"
+      strokeOpacity="0.6"
+      strokeWidth="10"
+    />
   );
 }
 
@@ -71,9 +81,8 @@ interface TreeProps {
   height: number;
   radius: number;
 }
-function Tree({width, height, radius}: TreeProps) {
-
-  const [simulation, ] = useState(forceSimulation().stop());
+function Tree({ width, height, radius }: TreeProps) {
+  const [simulation] = useState(forceSimulation().stop());
   const [colors, setColors] = useState<Colors>();
   const [nodes, setNodes] = useState<Person[]>([]);
   const [links, setLinks] = useState<PersonLink[]>([]);
@@ -105,11 +114,11 @@ function Tree({width, height, radius}: TreeProps) {
       simulation.nodes(people);
       simulation
         .force('charge', forceManyBody().strength(-300))
-        .force('link', forceLink<Person, PersonLink>(links).id(d => d.id).strength(0.05))
+        .force('link', forceLink<Person, PersonLink>(links).id((d) => d.id).strength(0.05))
         .force('age', ageOrdering(height, 0.1))
         .force('horizontal-center', forceX(width / 2).strength(0.05));
       simulation.alphaTarget(0.3).restart();
-    })
+    });
   }, []);
 
   simulation.on('tick', () => {
@@ -120,21 +129,25 @@ function Tree({width, height, radius}: TreeProps) {
   const [drag, setDrag] = useState<[number, number, Person] | null >(null);
 
   return (
-    <svg width={width} height={height} style={{border: '1px solid black'}}
-  onMouseMove={(e) => {
-      if (drag) {
-        const [offsetX, offsetY, person] = drag;
+    <svg
+      width={width}
+      height={height}
+      style={{ border: '1px solid black' }}
+      onMouseMove={(e) => {
+        if (drag) {
+          const [offsetX, offsetY, person] = drag;
         person.fx! = e.clientX + offsetX;
         person.fy! = e.clientY + offsetY;
-      }}}
-      onMouseUp={()=>{
+        }
+      }}
+      onMouseUp={() => {
         if (drag) {
           const [, , person] = drag;
           person.fx = person.fy = null;
           setDrag(null);
         }
       }}
-      onMouseLeave={()=>{
+      onMouseLeave={() => {
         if (drag) {
           const [, , person] = drag;
           person.fx = person.fy = null;
@@ -143,20 +156,19 @@ function Tree({width, height, radius}: TreeProps) {
       }}
     >
       <g>
-        {links.map(link =>
-          <PersonLinkSVG link={link} />
-        )}
+        {links.map((link) => <PersonLinkSVG link={link} />)}
       </g>
       <g>
-        {nodes.map(node =>
-        <g onMouseDown={(e)=>{
-          setDrag([node.x! - e.clientX, node.y! - e.clientY, node])
-        }}
-          cursor='move'
-        >
-          <PersonSVG person={node} radius={radius} reignColors={colors!.reign} />
-        </g>
-        )}
+        {nodes.map((node) => (
+          <g
+            onMouseDown={(e) => {
+              setDrag([node.x! - e.clientX, node.y! - e.clientY, node]);
+            }}
+            cursor="move"
+          >
+            <PersonSVG person={node} radius={radius} reignColors={colors!.reign} />
+          </g>
+        ))}
       </g>
     </svg>
   );
