4497412f67655487d94463dc19adde044ee79d2c
Andrew Lee <andrew.lambda@tuta.io>
2021-08-29 03:07:21 -0400

    chore: remove unused code

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState, MouseEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   create,
-  selectAll,
   drag
 } from 'd3';
 import {
@@ -16,7 +15,6 @@ import { flatMap } from 'lodash';
 import {
   Person, PersonLink, DataSchema, Reign, Color, Colors
 } from '../utils/types';
-import { Draggable } from '@shopify/draggable';
 
 export function ageOrdering(height: number, strength: number = 0.1): Force<Person, PersonLink> {
   let nodes: Person[],
@@ -47,116 +45,6 @@ export function ageOrdering(height: number, strength: number = 0.1): Force<Perso
   };
 
   return force;
-}
-
-const chart = ({ people, colors }: DataSchema, width = 600, height = 800) => {
-
-  const radius = 20;
-  const nodes = people;
-
-  const links = flatMap(people, (person: Person) => {
-    const result = [];
-    if (person.mother) {
-      result.push({
-        source: person.id,
-        target: person.mother
-      });
-    }
-
-    if (person.father) {
-      result.push({
-        source: person.id,
-        target: person.father
-      });
-    }
-
-    return result;
-  });
-
-  const svg = create('svg').attr('viewBox', `0 0 ${width} ${height}`)
-
-  const simulation = forceSimulation(nodes)
-    .force('charge', forceManyBody().strength(-300))
-    .force('link', forceLink<Person, PersonLink>(links).id(d => d.id).strength(0.05))
-    .force('age', ageOrdering(height, 0.1))
-    .force('horizontal-center', forceX(width / 2).strength(0.05));
-
-  const link = svg.append("g")
-    .selectAll("line")
-    .data(links)
-    .join("line")
-    .attr("stroke", "#999")
-    .attr("stroke-opacity", 0.6)
-    .attr("stroke-width", 10);
-
-  const container = svg.append("g")
-
-  const masks = container
-    .selectAll('clipPath')
-    .data(nodes)
-    .join('clipPath')
-    .attr('id', d => `${d.id}-mask`)
-    .append('circle')
-    .attr('r', radius)
-  .attr('stroke', '#000')
-
-  const flags = container
-    .selectAll()
-    .data(nodes)
-    .join('image')
-    .attr('href', d => `/flags/${d.country}.svg`)
-    .attr('height', radius * 2)
-    .attr('clip-path', d => `url(#${d.id}-mask)`)
-
-  const outlines = container
-    .selectAll()
-    .data(nodes)
-    .join('circle')
-    .attr('r', radius)
-    .attr('stroke', (d: Person) => d.reign ? colors.reign[d.reign] : colors.reign.none)
-    .attr('stroke-width', '4px')
-    .attr('fill-opacity' , 0)
-  // @ts-ignore
-    .call(drag(simulation));
-
-  const text = container
-    .selectAll('text')
-    .data(nodes)
-    .join('text')
-    .text(d => d.name)
-    .attr('stroke', '#fff')
-    .attr('stroke-width', 0.5)
-    .attr('stroke-opacity', 0.6)
-    .attr('fill', '#000')
-    .attr('font-size', '16px')
-
-
-  simulation.on("tick", () => {
-    link
-    // @ts-expect-error
-        .attr("x1", d => d.source.x)
-    // @ts-expect-error
-        .attr("y1", d => d.source.y)
-    // @ts-expect-error
-        .attr("x2", d => d.target.x)
-    // @ts-expect-error
-        .attr("y2", d => d.target.y);
-
-    flags
-        .attr("x", d => d.x! - 2 * radius)
-        .attr("y", d => d.y! - radius);
-    text
-      .attr('x', d => d.x! - radius)
-      .attr('y', d => d.y! - radius)
-    masks
-      .attr('cx', d => d.x!)
-      .attr('cy', d => d.y!)
-    outlines
-      .attr('cx', d => d.x!)
-      .attr('cy', d => d.y!)
-  });
-
-  return svg.node();
 }
 
 async function fetchData(): Promise<DataSchema> {
