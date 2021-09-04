2919cc4245dd2fc4d65b61507ecec05da96c7343
Andrew Lee <andrew.lambda@tuta.io>
2021-09-04 02:15:32 -0400

    feat: add navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Tree from './Tree';
@@ -27,34 +29,45 @@ function App() {
   const [config, setConfig] = useState(defaultConfig);
 
   return (
-    <Container>
-      <Row>
-        <h1>Hereditary</h1>
-        <hr />
-      </Row>
-        <Async promiseFn={() => fetchData(join(process.env.PUBLIC_URL, 'data.yml'))} >
-          <Async.Fulfilled>
-            {(data: DataSchema) =>
-            <>
-              <Row>
-                <Accordion style={{marginBottom: '5px'}} defaultActiveKey='0'>
-                  <Accordion.Item eventKey='0'>
-                    <Accordion.Header>Options</Accordion.Header>
-                    <Accordion.Body>
-                      <ConfigEditor data={data} config={config} submitConfig={setConfig} />
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
-              </Row>
-              <Row>
-                <Tree data={data} config={config} />
-              </Row>
-            </>
-            }
-          </Async.Fulfilled>
-          <Async.Loading>Loading data...</Async.Loading>
-        </Async>
-    </Container>
+    <>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href='/hereditary'>Hereditary</Navbar.Brand>
+          <Nav>
+            <Nav.Link href=''>GitHub</Nav.Link>
+            <Nav.Link href=''>File an Issue</Nav.Link>
+            <Nav.Link href=''>Contribute Data</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <hr />
+        </Row>
+          <Async promiseFn={() => fetchData(join(process.env.PUBLIC_URL, 'data.yml'))} >
+            <Async.Fulfilled>
+              {(data: DataSchema) =>
+              <>
+                <Row>
+                  <Accordion style={{marginBottom: '5px'}} defaultActiveKey='0'>
+                    <Accordion.Item eventKey='0'>
+                      <Accordion.Header>Options</Accordion.Header>
+                      <Accordion.Body>
+                        <ConfigEditor data={data} config={config} submitConfig={setConfig} />
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </Row>
+                <Row>
+                  <Tree data={data} config={config} />
+                </Row>
+              </>
+              }
+            </Async.Fulfilled>
+            <Async.Loading>Loading data...</Async.Loading>
+          </Async>
+      </Container>
+    </>
   );
 }
 
