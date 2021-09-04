e54ee5c5e31ef7c2c1d532fc85d743392ec70567
Andrew Lee <andrew.lambda@tuta.io>
2021-09-04 17:17:39 -0400

    feat: add version to navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import { repository } from '../../package.json';
+import { repository, version } from '../../package.json';
 import { join } from 'path';
 
 export default function Navigation() {
   return (
     <Navbar>
       <Container>
-        <Navbar.Brand href='/hereditary'>Hereditary</Navbar.Brand>
+        <Nav>
+          <Navbar.Brand href='/hereditary'>Hereditary</Navbar.Brand>
+          <Navbar.Text>v{version}</Navbar.Text>
+        </Nav>
         <Nav>
           <Nav.Link href={repository}>GitHub</Nav.Link>
           <Nav.Link href={join(repository, 'issues/new')}>File an Issue</Nav.Link>
